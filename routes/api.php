1477662620241d8d68a66107ba05b168f0c80555
Polvere243 <danielercoli@gmail.com>
2024-04-11 08:39:19 +0200

    fixed something

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{project}', [ProjectController::class, 'show']);
 Route::post('/projects', [ProjectController::class, 'store']);
-Route::get('/projects', [ProjectController::class, 'index']);
 Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
 Route::put('/projects/{project}', [ProjectController::class, 'update']);
