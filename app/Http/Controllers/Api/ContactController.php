18878a46d973b5cbe0c9cca65fe91ed17aa8c497
Polvere243 <danielercoli@gmail.com>
2024-04-11 15:27:26 +0200

    imported the mailable and the facades to send the email

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Mail\ContactMessageMail;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     public function message()
     {
+        $email = new ContactMessageMail();
+
         return response()->json('ciao');
     }
 }
