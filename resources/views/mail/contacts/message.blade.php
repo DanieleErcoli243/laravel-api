1003325a1528357ac139d797ee28a66cb0e15436
Polvere243 <danielercoli@gmail.com>
2024-04-11 08:59:22 +0200

    stylized the blade for the e-mail

---
+++ resources/views/mail/contacts/message.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME') }} | e-mail</title>
+    <style>
+        body{
+            padding: 50px;
+            text-align: center;
+            background-color: lightgray;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>È stato inviato un messaggio</h1>
+</body>
+</html>
